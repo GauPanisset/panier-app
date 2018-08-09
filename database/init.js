deb4d5d4cdc0dc55559d42ab9c14a354875188e9
GauPanisset <gauthier.panisset@depinfonancy.net>
2018-08-09 15:35:24 +0200

    heroku

--- database/init.js
+++ database/init.js
@@ -26,22 +26,24 @@ if (url !== undefined) {
 let data;
 
 function handleDisconnect() {
-    data = mysql.createConnection(config); // Recreate the connection, since
-                                                    // the old one cannot be reused.
+    data = mysql.createConnection(config);
 
-    data.connect(function(err) {              // The server is either down
-        if(err) {                                     // or restarting (takes a while sometimes).
+
+    data.connect(function(err) {
+        if(err) {
             console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-        }                                     // to avoid a hot loop, and to allow our node script to
-    });                                     // process asynchronous requests in the meantime.
-                                            // If you're also serving http, display a 503 error.
+            handleDisconnect();
+        } else {
+            console.log('connected');
+        }
+    });
+
     data.on('error', function(err) {
         console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-            handleDisconnect();                         // lost due to either server restart, or a
-        } else {                                      // connnection idle timeout (the wait_timeout
-            throw err;                                  // server variable configures this)
+        if(err.code === 'PROTOCOL_CONNECTION_LOST' || err.fatal) {
+            handleDisconnect();
+        } else {
+            throw err;
         }
     });
 }
